84b7f50eeac67fb63208c7ad7eef68787bec04e1
amrit110 <amrit110@gmail.com>
2023-03-02 12:08:49 -0500

    1 file modified

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -18,6 +18,7 @@ const config = {
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'VectorInstitute', // Usually your GitHub org/user name.
   projectName: 'cyclops', // Usually your repo name.
+  deploymentBranch: 'github_pages', // Branch that GitHub pages will deploy from.
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
